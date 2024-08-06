58eec19e0fe77591fce7e7c88636819c25a0aabb
Lukio132 <bolisega132@gmail.com>
2024-08-06 11:44:34 +0200

    Initial Commit

---
+++ SimpleWeather/SimpleWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleWeatherApp.swift
+//  SimpleWeather
+//
+//  Created by Łukasz Bolisęga on 06/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct SimpleWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
